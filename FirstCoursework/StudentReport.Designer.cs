c60bf6030547fb53d073d8602c299077cdf7fb9b
Pranaya Pradhan <42856837+Pranaya01@users.noreply.github.com>
2020-01-02 21:22:31 +0545

    New Files

---
+++ FirstCoursework/StudentReport.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace FirstCourseWork
+{
+    partial class StudentReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Name = "ReportViewer";
+            this.reportViewer.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer.TabIndex = 0;
+            // 
+            // StudentReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "StudentReport";
+            this.Text = "StudentReport";
+            this.Load += new System.EventHandler(this.StudentReport_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+    }
+}
---
+++ FirstCoursework/StudentReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstCourseWork
+{
+    public partial class StudentReport : Form
+    {
+        public StudentReport()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void StudentReport_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
